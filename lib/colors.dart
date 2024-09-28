06802b0e60bf87cd73759df39eb7cc45d96a20f6
pawanbaghel01 <baghelpawan638@gmail.com>
2024-09-29 01:28:11 +0530

    Add colors done

---
+++ lib/colors.dart
@@ -0,0 +1,19 @@
+
+import 'package:flutter/material.dart';
+
+ // for light theme color scheme
+ const  lightBgColor = Color(0xffD1D9E6);
+ const  lightTextColor = Color(0xff246AEF);
+ const  lightDivColor = Color(0xffffffff);
+ const  lightLableColor = Color(0xff8C8C8C8C);
+ 
+  // common color 
+ const  darkColor = Color(0xff000000);
+ const  lightColor = Color(0xffffffff);
+ const  buttonColor = Color(0xff246AEF);
+
+  // for light theme color scheme
+ const  darkBgColor = Color(0xffD1D9E6);
+ const  darkTextColor = Color(0xff246AEF);
+ const  darkDivColor = Color(0xff373737);
+ const  darkLableColor = Color(0xff8C8C8C8C);
